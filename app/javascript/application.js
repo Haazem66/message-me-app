fd4cd31e48afcb0096511b8721e6c64f441a5223
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2022-09-15 23:28:48 +0200

    added navigation bar, login page and home page, user and messages resources

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery
+//= require semantic-ui
 import "@hotwired/turbo-rails"
+import * as jquery from "jquery"
+import "semantic-ui" 
 import "controllers"
+$(function() {     $('.ui.dropdown').dropdown(); }); 
+// $(document).on('turbo:load', function(){ //     $('.ui.dropdown').dropdown(); // }) 
