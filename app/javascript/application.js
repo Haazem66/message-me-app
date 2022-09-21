f053ce18a022edad7389a12871cff184ec5d33e5
Haazem66 <61153924+Haazem66@users.noreply.github.com>
2022-09-21 23:46:08 +0200

    final touches

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,14 @@ import "@hotwired/turbo-rails"
 import * as jquery from "jquery"
 import "semantic-ui" 
 import "controllers"
+
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 $(function() {     $('.ui.dropdown').dropdown(); 
 
 }); 
@@ -17,4 +25,5 @@ $('.message .close')
     ;
   })
 ;
-// $(document).on('turbo:load', function(){ //     $('.ui.dropdown').dropdown(); // }) 
+scroll_bottom();
+// $(document).on('turbo:load', function(){ //     $('.ui.dropdown').dropdown(); // }) import "channels"
